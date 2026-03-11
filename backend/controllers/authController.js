3c9815cba94dc773d2309bb328da097d36ba3f7c
Paras Jain <guru67904@gmail.com>
2026-03-12 01:40:09 +0530

    fix cookies for cross domain

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -27,11 +27,13 @@ export const register = async (req,res)=>{
 
         const token = generateToken(user._id)
 
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:true,
-            sameSite:"lax"
-        })
+      res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
+
 
         res.status(201).json({
             message:"Signup Success",
@@ -72,11 +74,13 @@ export const login = async (req,res)=>{
 
         const token = generateToken(user._id)
 
-        res.cookie("token",token,{
-            httpOnly:true,
-            secure:true,
-            sameSite:"lax"
-        })
+       res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 7 * 24 * 60 * 60 * 1000
+})
+
 
         // check if user already in room
         if(user.room){
