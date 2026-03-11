6437cb91033569f06d15557c0874b4aa80dfb4b1
Paras Jain <guru67904@gmail.com>
2026-03-12 01:34:43 +0530

    fix cookies for production

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,7 +29,7 @@ export const register = async (req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
+            secure:true,
             sameSite:"lax"
         })
 
@@ -74,7 +74,7 @@ export const login = async (req,res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
+            secure:true,
             sameSite:"lax"
         })
 
