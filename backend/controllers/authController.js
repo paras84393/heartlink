dddade2adbf3bee40435d5cc2c852b61e25a8a5c
Paras Jain <guru67904@gmail.com>
2026-03-13 00:34:30 +0530

    fix vercel routing

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -26,13 +26,14 @@ export const register = async (req,res)=>{
         })
 
         const token = generateToken(user._id)
-res.cookie("token", token, {
+
+      res.cookie("token", token, {
   httpOnly: true,
   secure: true,
   sameSite: "none",
-  path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000
 })
+
 
         res.status(201).json({
             message:"Signup Success",
@@ -73,11 +74,10 @@ export const login = async (req,res)=>{
 
         const token = generateToken(user._id)
 
-res.cookie("token", token, {
+       res.cookie("token", token, {
   httpOnly: true,
   secure: true,
   sameSite: "none",
-  path: "/",
   maxAge: 7 * 24 * 60 * 60 * 1000
 })
 
